71715ca73de0138e83435e077766b37e7ab4f4fd
HarshvardhanDhabhai <harshvardhandhabhai2004@gmail.com>
2025-08-21 19:56:48 +0530

    Auto-update all files on 2025-08-21 19:56:48

--- p4.py
+++ p4.py
@@ -3,3 +3,15 @@ age = 100
 height = 6.2
 print(name, age, height)
 ''' this is a multi-line comment '''
+a = 10
+b=10.4
+c = 3+5j
+print(type(a),type(b),type(c))
+my_list  = ['data1','data2','data3']
+my_tuple = ('data1','data2','data3')
+print(my_tuple)
+print(my_list[0])
+set_no = {1124,2416,1,2,3,44,5,55,5,632,24,1,2}
+print(set_no)
+vegetables  = ('Karela','Aloo','Tamatar')
+print('Bhindi' not in vegetables)
